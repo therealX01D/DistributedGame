057d45fa3c383e6050d68e6605a7fa6970f6de65
Mahmoud Elkhashab <mahmoudelkhashab26@gmail.com>
2023-05-16 03:39:35 +0300

    created an alpha branch to avoid non-tested functions

--- server.py
+++ server.py
@@ -2,14 +2,27 @@
 
 import asyncio
 from websockets.server import serve
+import json
 
-async def echo(websocket):
+async def error(websocket, message):
+    event = {
+        "type": "error",
+        "message": message,
+    }
+    await websocket.send(json.dumps(event))
+
+async def Handler(websocket):
     async for message in websocket:
+        dic=json.loads(message)
+        if dic["type"]=="init":
+            #todo: create a join key and return it
+            pass
         print(message)
         await websocket.send(message)
 
 async def main():
-    async with serve(echo, "localhost", 8765):
+    async with serve(Handler, "localhost", 8765):
         await asyncio.Future()  # run forever
+        
 
 asyncio.run(main())
