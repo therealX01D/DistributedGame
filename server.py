4b41cbb9ee103a94cf5958d7f1ea1c8b57d666dc
Omar Ayman <75280540+oaayoub@users.noreply.github.com>
2023-05-27 10:12:33 +0300

    SERVER thread

---
+++ server.py
@@ -0,0 +1,25 @@
+import websockets
+import asyncio
+import pygame
+
+IPADDRESS = "localhost"
+PORT = 1761
+
+EVENTTYPE = pygame.event.custom_type()
+
+# handler processes the message and sends "Success" back to the client
+async def handler(websocket, path):
+    async for message in websocket:
+        await processMsg(message)
+        await websocket.send("Success")
+
+async def processMsg(message):
+    print(f"[Received From Client]: {message}")
+    pygame.fastevent.post(pygame.event.Event(EVENTTYPE, message=message))
+
+async def main(future):
+    async with websockets.serve(handler, IPADDRESS, PORT):
+        await future  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
