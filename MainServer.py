4441defe8566c74c31f14290e0dc032fb5cd6a84
oaayoub <75280540+oaayoub@users.noreply.github.com>
2023-06-05 01:30:02 +0300

    Fixed MainServer

--- MainServer.py
+++ MainServer.py
@@ -46,7 +46,7 @@ class AbstractCar:
         self.vel = 0
         self.rotation_vel = rotation_vel
         self.angle = 0
-        self.x, self.y = self.START_POS
+        self.x, self.y = (0,0)
         self.acceleration = 0.05
 
     def rotate(self, left=False, right=False):
