325e1ea20245260d78da916041f07034f5fb768d
oaayoub <75280540+oaayoub@users.noreply.github.com>
2023-06-05 19:26:17 +0300

    added user adds his username

--- MainServer.py
+++ MainServer.py
@@ -5,7 +5,7 @@ from Server import main
 import zmq
 context = zmq.Context()
 if __name__ == "__main__" :
-    ServerProcess = mp.Process(target=main)
+    ServerProcess = mp.Process(target=main,args=(2,))
     puller = context.socket(zmq.PULL)
     puller.connect("tcp://localhost:"+str(20211))
     while True:
