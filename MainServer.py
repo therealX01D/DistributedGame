df603f4987d6fc0d3e4ebba30de37d76c0104008
oaayoub <75280540+oaayoub@users.noreply.github.com>
2023-06-05 19:01:36 +0300

    added user adds his username

--- MainServer.py
+++ MainServer.py
@@ -103,8 +103,8 @@ class PlayerCar(AbstractCar):
         self.vel = -self.vel/1.6
         self.move()
 
-player1 = PlayerCar(4,4,0,(150,210))
-player2 = PlayerCar(4,4,1,(180,210))
+player1 = PlayerCar(4,4,0,(170,210))
+player2 = PlayerCar(4,4,1,(190,210))
 arr_players_class = [player1,player2]
 ##END: PYGAME ASSETS
 
